e984887b12333c82467b75261cc1686f23180e3a
Jobwawesh <jobnjunge15@gmail.com>
2023-02-26 23:49:27 +0300

    created initial README

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
-puts "Seeding tables..."
+puts "Seeding data..."
 
-10.times do 
+
+5.times do 
     user =  User.create!(name: Faker::Name.name)
 end
 
@@ -8,11 +9,11 @@ random_user = (1..10).to_a.sort{ raand() - 0.5}
 
 5.times do 
     product = Product.create(name: Faker::Commerce.product_name)
-    rand(1..5).times do |i|
+    rand(1..10).times do |i|
         Review.create(
             star_rating: rand(1..5),
             comment: Faker::Lorem.sentence,
-            product: product
+            product: product,
             user: random_user[i]
         )
     end
